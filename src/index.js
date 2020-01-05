0050948110f2319adf4ce09c06dfa74d671da71e
coolworld2015 <wintermute2015@ukr.net>
2020-01-05 18:11:05 +0200

    added medium

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 
-import UsersApp from './react/UserApp';
-import ReduxApp from './redux/index';
+//import UsersApp from './react/UserApp';
+//import ReduxApp from './redux/index';
 
-ReactDOM.render(<ReduxApp />, document.getElementById('root'));
+import MediumApp from './medium/index';
+
+ReactDOM.render(<MediumApp />, document.getElementById('root'));
