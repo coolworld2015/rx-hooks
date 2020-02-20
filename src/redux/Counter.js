9f82838002e2236feedffc7e5672abefa80860a4
coolworld2015 <wintermute2015@ukr.net>
2020-02-20 21:28:45 +0200

    added material-ui

--- src/redux/Counter.js
+++ src/redux/Counter.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import { store } from "./store";
 import { incrementThunk, decrementThunk } from "./actions";
 import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 
 const action = type => store.dispatch({ type });
 
 export const Counter = () => {
     const dispatch = useDispatch();
     const counter = useSelector(state => state.value);
+    const [description, setDescription] = useState('');
+    const handleChangeDescription = (
+        e
+    ) => {
+        setDescription(e.target.value);
+    };
+
     return (
         <div style={{textAlign: 'center'}}>
             <h1>{counter}</h1>
@@ -36,6 +44,15 @@ export const Counter = () => {
             >
                 DECREMENT
             </Button>
+
+            <TextField
+                multiline={true}
+                rows={1}
+                label={'descriptionCapital'}
+                value={description}
+                onChange={handleChangeDescription}
+
+            />
         </div>
     );
 };
